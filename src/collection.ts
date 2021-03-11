c2390712aaaccf426708e49805e511dcbd6ad388
ostrebler <strebler.olivier@hotmail.fr>
2021-03-11 08:41:19 +0100

    Added more methods to Collection

--- src/collection.ts
+++ src/collection.ts
@@ -8,10 +8,12 @@ import {
   MongoCountPreferences,
   OptionalId
 } from "mongodb";
-import { map } from "lodash";
-import { Database, FilterQuery } from ".";
+import { get, map } from "lodash";
+import { Database, FilterQuery, normalizeFilterQuery } from ".";
 
-export class Collection<T extends object> {
+export type DocumentT = Record<string, any>;
+
+export class Collection<T extends DocumentT> {
   database: Database;
   name: string;
   handle: Promise<Col<T>>;
@@ -26,53 +28,82 @@ export class Collection<T extends object> {
     this.handle = database.handle.then(db => db.collection<T>(name, options));
   }
 
-  aggregate<U = T>(
+  // Query methods :
+
+  async aggregate<U = T>(
     pipeline?: object[],
     options?: CollectionAggregationOptions
   ) {
-    return this.handle.then(col =>
-      col.aggregate<U>(pipeline, options).toArray()
-    );
+    const col = await this.handle;
+    return col.aggregate<U>(pipeline, options).toArray();
   }
 
-  // Query methods :
-
-  find<U = T>(
-    query: FilterQuery<T>,
-    options?: FindOneOptions<U extends T ? T : U>
-  ) {
-    return this.handle.then(col => col.find(query, options ?? {}).toArray());
+  async count(query: FilterQuery<T> = {}, options?: MongoCountPreferences) {
+    const col = await this.handle;
+    const normalized = await normalizeFilterQuery(this, query);
+    return col.countDocuments(normalized, options);
   }
 
-  findMap<U = T>(
-    query: FilterQuery<T>,
-    key: keyof U,
+  async find<U = T>(
+    query: FilterQuery<T> = {},
     options?: FindOneOptions<U extends T ? T : U>
   ) {
-    return this.find(query, {
+    const col = await this.handle;
+    const normalized = await normalizeFilterQuery(this, query);
+    return col.find(normalized, options ?? {}).toArray();
+  }
+
+  async findOne<U = T>(
+    query: FilterQuery<T> = {},
+    options?: FindOneOptions<U extends T ? T : U>
+  ) {
+    const col = await this.handle;
+    const normalized = await normalizeFilterQuery(this, query);
+    return col.findOne(normalized, options);
+  }
+
+  async findMap<U = T, K extends keyof U = keyof U>(
+    query: FilterQuery<T>,
+    key: K,
+    options?: FindOneOptions<U extends T ? T : U>
+  ) {
+    const array = await this.find(query, {
       ...options,
       fields: { [key]: 1 } as any
-    }).then(array => map(array, key));
+    });
+    return map(array, key);
   }
 
-  count(query?: FilterQuery<T>, options?: MongoCountPreferences) {
-    return this.handle.then(col => col.countDocuments(query, options));
+  async findOneMap<U = T, K extends keyof U = keyof U>(
+    query: FilterQuery<T>,
+    key: K,
+    options?: FindOneOptions<U extends T ? T : U>
+  ) {
+    const document = await this.findOne(query, {
+      ...options,
+      fields: { [key]: 1 } as any
+    });
+    if (document === null)
+      throw new Error(
+        `Could not find document in collection <${this.name}>.findOneMap`
+      );
+    return get(document, key) as U[K];
   }
 
   // Insert methods :
 
-  insertOne(doc: OptionalId<T>, options?: CollectionInsertOneOptions) {
-    return this.handle
-      .then(col => col.insertOne(doc, options))
-      .then(({ ops }) => ops[0]);
+  async insertOne(doc: OptionalId<T>, options?: CollectionInsertOneOptions) {
+    const col = await this.handle;
+    const result = await col.insertOne(doc, options);
+    return result.ops[0];
   }
 
-  insertMany(
+  async insertMany(
     docs: Array<OptionalId<T>>,
     options?: CollectionInsertManyOptions
   ) {
-    return this.handle
-      .then(col => col.insertMany(docs, options))
-      .then(({ ops }) => ops);
+    const col = await this.handle;
+    const result = await col.insertMany(docs, options);
+    return result.ops;
   }
 }
