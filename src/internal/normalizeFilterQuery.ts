2ae748e0c7da002f57a2179703901afaefe2b736
ostrebler <strebler.olivier@hotmail.fr>
2021-03-15 21:27:12 +0100

    Better code

--- src/internal/normalizeFilterQuery.ts
+++ src/internal/normalizeFilterQuery.ts
@@ -18,28 +18,30 @@ export function normalizeFilterQuery<T extends Document>(
   return mapDeep(query, async function customizer(value, stack, parent) {
     if (isAugmentedSelector(value)) {
       const key = stackToKey(stack);
-      let { $in, $nin, $$in, $$nin, $$eq, $$ne, ...props } = value;
+      // Get the foreign key config
+      const foreignKeyConfig = collection.foreignKeys[key];
+      if (!foreignKeyConfig)
+        throw new Error(
+          `No foreign key is set for <${key}> in collection <${collection.name}>`
+        );
+      // Get the foreign collection
+      const foreignCol = collection.rongo.collection(
+        foreignKeyConfig.collection
+      );
+      // This function returns a list of possible foreign keys given a foreign filter :
+      const foreignKeys = (query: FilterQuery<any>, unique?: boolean) =>
+        foreignCol.findResolve(
+          query,
+          foreignCol.primaryKey,
+          unique ? { limit: 1 } : undefined
+        );
       // If there is any augmented operator, they get merged with regular operators :
-      if ($$in)
-        $in = [
-          ...($in ? $in : []),
-          ...(await findForeignKeys(collection, key, $$in))
-        ];
-      if ($$nin)
-        $nin = [
-          ...($nin ? $nin : []),
-          ...(await findForeignKeys(collection, key, $$nin))
-        ];
-      if ($$eq)
-        $in = [
-          ...($in ? $in : []),
-          ...(await findForeignKeys(collection, key, $$eq, true))
-        ];
+      let { $in, $nin, $$in, $$nin, $$eq, $$ne, ...props } = value;
+      if ($$in) $in = [...($in ? $in : []), ...(await foreignKeys($$in))];
+      if ($$nin) $nin = [...($nin ? $nin : []), ...(await foreignKeys($$nin))];
+      if ($$eq) $in = [...($in ? $in : []), ...(await foreignKeys($$eq, true))];
       if ($$ne)
-        $nin = [
-          ...($nin ? $nin : []),
-          ...(await findForeignKeys(collection, key, $$ne, true))
-        ];
+        $nin = [...($nin ? $nin : []), ...(await foreignKeys($$ne, true))];
       return {
         // The rest still needs to get recursively checked :
         ...(await mapDeep(props, customizer, stack, parent)),
@@ -56,29 +58,5 @@ export function isAugmentedSelector(entity: any): entity is QuerySelector<any> {
   return (
     isPlainObject(entity) &&
     (entity.$$in || entity.$$nin || entity.$$eq || entity.$$ne)
-  );
-}
-
-// This function returns a list of possible foreign keys given a collection, one of its foreign key,
-// and a filter query for the foreign collection
-
-export function findForeignKeys<T extends Document>(
-  collection: Collection<T>,
-  foreignKey: string,
-  foreignQuery: FilterQuery<any>,
-  unique?: boolean
-) {
-  // Get the foreign key config
-  const foreignKeyConfig = collection.foreignKeys[foreignKey];
-  if (!foreignKeyConfig)
-    throw new Error(
-      `No foreign key is set for <${foreignKey}> in collection <${collection.name}>`
-    );
-  // Get the primary keys of the targeted foreign documents :
-  const foreignCol = collection.rongo.collection(foreignKeyConfig.collection);
-  return foreignCol.findResolve(
-    foreignQuery,
-    foreignCol.primaryKey,
-    unique ? { limit: 1 } : undefined
   );
 }
