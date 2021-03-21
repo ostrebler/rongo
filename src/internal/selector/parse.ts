4f813b0ac84f00601773eeee4a3a7ca8ead9e46c
ostrebler <strebler.olivier@hotmail.fr>
2021-03-21 23:11:23 +0100

    Intermediary state

--- src/internal/selector/parse.ts
+++ src/internal/selector/parse.ts
@@ -1,6 +1,23 @@
-import { SelectArgument, Selector } from ".";
+import { SelectArgument } from ".";
 
-export declare function parse(
+// This is the grammar of Selectors :
+//
+// selector:
+// | <field> selector                        { FieldSelector(field, arg, selector) }
+// | <index> selector                        { IndexSelector(index, selector) }
+// | <$> selector                            { MapSelector(selector) }
+// | <$$> selector                           { FlatMapSelector(selector) }
+// | <arg as object> selector                { FilterSelector(arg, selector) }
+// | <arg as function> selector              { PredicateSelector(arg, selector) }
+// | <arg as selector>                       { arg }
+// | <[> (selector <,>?)* <]>                { TupleSelector(...[selector]) }
+// | <{> ((<field>|<*>) selector <,>?)* <}>  { ObjectSelector(...[field, selector]) }
+// | <>                                      { IdentitySelector }
+//
+// spacing:
+// | <[.\s]+>
+
+export function parse(
   raw: string,
-  symtable?: Map<string, SelectArgument>
-): Selector;
+  symTable: Map<string, SelectArgument> = new Map()
+) {}
--- src/internal/selector/types.ts
+++ src/internal/selector/types.ts
@@ -16,3 +16,43 @@ export type SelectArgument =
   | Selector
   | FilterQuery<any>
   | PredicateSelectorCallback;
+
+// Used to label a specific token in order to be consumed by the parser :
+
+export enum SelectorTokenType {
+  Field,
+  Wildcard,
+  Index,
+  Comma,
+  MapOperator,
+  FlatMapOperator,
+  SelectArgument,
+  BracketOpen,
+  BracketClose,
+  SquareBracketOpen,
+  SquareBracketClose
+}
+
+// Use as a yield value by the token generator :
+
+export type SelectorToken =
+  | {
+      type: SelectorTokenType.Field;
+      field: string;
+    }
+  | {
+      type: SelectorTokenType.Index;
+      index: number;
+    }
+  | {
+      type: SelectorTokenType.SelectArgument;
+      argument: SelectArgument;
+    }
+  | {
+      type: Exclude<
+        SelectorTokenType,
+        | SelectorTokenType.Field
+        | SelectorTokenType.Index
+        | SelectorTokenType.SelectArgument
+      >;
+    };
