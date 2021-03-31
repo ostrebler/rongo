0c4d72d7da4d662b52deab30f9afe93256bb190e
ostrebler <strebler.olivier@hotmail.fr>
2021-03-31 14:25:11 +0200

    Better normalizeFilterQuery

--- src/internal/normalizeFilterQuery.ts
+++ src/internal/normalizeFilterQuery.ts
@@ -13,54 +13,57 @@ export function normalizeFilterQuery<T extends Document>(
     if (!isPlainObject(value)) return;
     // Splitting the object in relevant parts :
     let { $expr, $in, $nin, ...rest } = value;
+    // Normalizing the remaining parts in any case :
+    const normalized: any = await mapDeep(rest, customizer, stack, parent);
+
     // If there's an $expr, it has to be ignored by the normalizing process :
-    if ($expr)
-      return {
-        $expr,
-        ...(await mapDeep(rest, customizer, stack, parent))
-      };
+    if ($expr) normalized.$expr = $expr;
 
-    // If there's no $in or $nin operator, then stick to standard deep mapping :
-    if (!$in && !$nin) return;
-    // Otherwise, check the current key :
-    const key = stackToKey(stack);
-    // Get the foreign key config if one exists :
-    const foreignKeyConfig = collection.foreignKeys[key];
-    // If there's nothing to consider :
-    if (!foreignKeyConfig) return;
-
-    // Otherwise, get the foreign collection :
-    const foreignCol = collection.rongo.collection(foreignKeyConfig.collection);
-
-    // This function transforms augmented $in-like values to regular values :
-    const normalizeQuerySelectorList = (list: unknown) => {
-      if (list === undefined) return undefined;
-      // If it's a foreign filter query :
-      if (isPlainObject(list))
-        return findPrimaryKeys(foreignCol, list as FilterQuery<any>);
-      // If it's an array of keys and/or foreign filter queries :
-      if (isArray(list))
-        return list.reduce<Promise<Array<any>>>(
-          async (acc, item) =>
-            isPlainObject(item)
-              ? [...(await acc), ...(await findPrimaryKeys(foreignCol, item))]
-              : [...(await acc), item],
-          Promise.resolve([])
+    // If there's an $in and/or $nin operator :
+    if ($in || $nin) {
+      // Check the current key :
+      const key = stackToKey(stack);
+      // Get the foreign key config if one exists :
+      const foreignKeyConfig = collection.foreignKeys[key];
+      // If the $in and/or $nin applies to a foreign key :
+      if (foreignKeyConfig) {
+        // Get the foreign collection :
+        const foreignCol = collection.rongo.collection(
+          foreignKeyConfig.collection
         );
-      throw new Error(
-        `Invalid query selector for foreign key <${key}> in collection <${collection.name}> : <$in> and <$nin> selectors must be arrays or foreign filter queries`
-      );
-    };
 
-    $in = await normalizeQuerySelectorList($in);
-    $nin = await normalizeQuerySelectorList($nin);
+        // This function transforms augmented $in-like values to regular values :
+        const normalizeQuerySelectorList = (list: unknown) => {
+          if (list === undefined) return undefined;
+          // If it's a foreign filter query :
+          if (isPlainObject(list))
+            return findPrimaryKeys(foreignCol, list as FilterQuery<any>);
+          // If it's an array of keys and/or foreign filter queries :
+          if (isArray(list))
+            return list.reduce<Promise<Array<any>>>(
+              async (acc, item) =>
+                isPlainObject(item)
+                  ? [
+                      ...(await acc),
+                      ...(await findPrimaryKeys(foreignCol, item))
+                    ]
+                  : [...(await acc), item],
+              Promise.resolve([])
+            );
+          throw new Error(
+            `Invalid query selector for foreign key <${key}> in collection <${collection.name}> : <$in> and <$nin> selectors must be arrays or foreign filter queries`
+          );
+        };
 
-    return {
-      // The rest still needs to get recursively checked :
-      ...(await mapDeep(rest, customizer, stack, parent)),
-      ...($in && { $in }),
-      ...($nin && { $nin })
-    };
+        $in = await normalizeQuerySelectorList($in);
+        $nin = await normalizeQuerySelectorList($nin);
+      }
+      // Put the selectors back to the final result :
+      if ($in) normalized.$in = $in;
+      if ($nin) normalized.$nin = $nin;
+    }
+
+    return normalized;
   });
 }
 
