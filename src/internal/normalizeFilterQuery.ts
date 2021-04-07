c7cbd86a593de145d0b65666e2b64e51e47aef92
ostrebler <strebler.olivier@hotmail.fr>
2021-04-07 23:44:33 +0200

    Better code

--- src/internal/normalizeFilterQuery.ts
+++ src/internal/normalizeFilterQuery.ts
@@ -23,31 +23,30 @@ export async function normalizeFilterQuery<T extends Document>(
         // Get the foreign key config if one exists :
         const foreignKeyConfig = collection.foreignKeys[key];
         // If we're at a foreign key location :
-        if (foreignKeyConfig) {
-          // Get the foreign collection :
-          const foreignCol = collection.rongo.collection(
-            foreignKeyConfig.collection
-          );
+        if (!foreignKeyConfig) return;
+        // Get the foreign collection :
+        const foreignCol = collection.rongo.collection(
+          foreignKeyConfig.collection
+        );
 
-          const primaryKeys = (query: FilterQuery<any>) =>
-            foreignCol.find(query).select(foreignCol.key);
+        const primaryKeys = (query: FilterQuery<any>) =>
+          foreignCol.find(query).select(foreignCol.key);
 
-          // If we have a foreign filter query :
-          if (isPlainObject(value)) return primaryKeys(value);
-          // If we have an array of keys and/or foreign filter queries :
-          if (isArray(value))
-            return value.reduce<Promise<Array<any>>>(
-              async (acc, item) =>
-                isPlainObject(item)
-                  ? [...(await acc), ...(await primaryKeys(item))]
-                  : [...(await acc), item],
-              Promise.resolve([])
-            );
-          // Otherwise, it's a misshaped query :
-          throw new Error(
-            `Invalid query selector for foreign key <${key}> in collection <${collection.name}> : <$in> and <$nin> selectors must be arrays or foreign filter queries`
+        // If we have a foreign filter query :
+        if (isPlainObject(value)) return primaryKeys(value);
+        // If we have an array of keys and/or foreign filter queries :
+        if (isArray(value))
+          return value.reduce<Promise<Array<any>>>(
+            async (acc, item) =>
+              isPlainObject(item)
+                ? [...(await acc), ...(await primaryKeys(item))]
+                : [...(await acc), item],
+            Promise.resolve([])
           );
-        }
+        // Otherwise, it's a misshaped query :
+        throw new Error(
+          `Invalid query selector for foreign key <${key}> in collection <${collection.name}> : <$in> and <$nin> selectors must be arrays or foreign filter queries`
+        );
     }
   });
 }
